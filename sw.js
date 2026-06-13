e3fed18967fd0d0a8cfa9c63032dd5c92c153892
yschai990423-tech <yschai990423@gmail.com>
2026-06-13 23:19:48 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const cacheName = 'island-empire-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
